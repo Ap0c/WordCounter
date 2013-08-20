4706202f530e6deb602b8466603d967ef49d54ae
Jamie <thejeriatric@googlemail.com>
2013-08-20 23:06:45 +0100

    GUI Added
    Added a GUI, using Tkinter, for ease of use. Allows the user to enter a
    file path and have the word count outputted via a message box.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 WordCounter
 ===========
 
-Command line python program that takes in a text file and returns a word count.
+GUI python program that takes in a text file and returns a word count.
 
 Currently only tested with plain text (.txt) files.
--- WordCounter.py
+++ WordCounter.py
@@ -2,11 +2,33 @@
 of words in that line. It then takes the length of the list to get the word
 count for that line. This is added to total word count for each line processed."""
 
-#Tally of words in file.
-totalwords = 0
+from Tkinter import *
+import tkMessageBox
 
-with open(raw_input('What is the address of the file?: ')) as f:
-	for lines in f:
-		totalwords =  totalwords + len(lines.split())
+root = Tk()
 
-print totalwords
+#Sets attributes of the main window.
+root.wm_title("Word Counter") #Sets window title.
+#root.geometry("300x300") #Sets window size.
+root.wm_resizable(0,0) #Prevents resizing of window.
+
+instructions = Label(root, text = "Please provide the file path below, then hit enter to get the word count.", padx = 10, pady = 5)
+instructions.pack()
+
+#Creates a text entry box for the file path.
+filepath = Entry(root, width=40)
+filepath.pack()
+
+#Performs the counting of text in the file.
+def count(event):
+	totalwords = 0
+
+	with open(filepath.get()) as f:
+		for lines in f:
+			totalwords =  totalwords + len(lines.split())
+
+	tkMessageBox.showinfo("Word Count", "Word Count: %s" % totalwords) #Produces message box with word count.
+
+filepath.bind("<Return>", count)
+
+root.mainloop()
